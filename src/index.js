38463f1e3ff99e583ff7f11522586e8051ea0602
mbale014 <195115650+mbale014@users.noreply.github.com>
2025-12-01 13:58:38 +0800

    feat:add navbar and hero styling to home; typography adjustment on some text

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+import "./styles.css";
 import loadHome from "./init-page.js"
 
 loadHome();
+
 
 console.log("Hello, world!!")
--- src/init-page.js
+++ src/init-page.js
@@ -17,10 +17,10 @@ export default function loadHome() {
     headlineDiv.classList.add('headline');
 
     const h1Headline = document.createElement('h1');
-    h1Headline.textContent = 'Delicious food, served fresh everyday';
+    h1Headline.textContent = 'Delicious Food, Served Fresh Everyday';
 
     const pHeadline = document.createElement('p');
-    pHeadline.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
+    pHeadline.textContent = 'We offer a variety of freshly cooked meals made with the best ingredients';
 
     const cta = document.createElement('div');
     const btnOrder = document.createElement('button');
@@ -36,8 +36,9 @@ export default function loadHome() {
     headlineDiv.appendChild(cta)
 
     const heroImgDiv = document.createElement('div');
+    heroImgDiv.id = 'hero-img';
     const heroImg = document.createElement('img');
-    heroImg.src = 'https://placehold.co/400x400';
+    heroImg.src = 'https://images.unsplash.com/photo-1571091655789-405eb7a3a3a8?q=80&w=500&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
     heroImg.alt = 'hero-image';
 
     heroImgDiv.appendChild(heroImg)
